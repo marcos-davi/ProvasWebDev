4349d10b5d1a15a4c1f2d83776235ebf54b5b582
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2022-10-28 19:27:46 +0200

    APP react redux

--- CursoJS/redux/react-redux-app/src/App.js
+++
@@ -1,8 +0,0 @@
-
-function App() {
-  return (
-    <h1>React Redux App</h1>
-  );
-}
-
-export default App;
---
+++ react-redux-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='nav bar navbar-dark bg-dark'>
+        <div className='container'>
+            <a className='navbar-brand' href="/">React Redux App</a>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
