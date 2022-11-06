a4bf7ebc71e58b7ed4aed695ae239293e70667fb
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2022-11-06 19:12:49 +0100

    Testes Api

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,20 +1,33 @@
+let wa =0; 
 function setup() {
     createCanvas(400, 400);
   }
   
   function draw() {
+    wa++;
     background(220);
+    if(wa > width || wa> height){
+        wa=0;
+    }
     
-    test(200,200, 100, 100);
-    test(150,150,100,100);
-    test(15,15,50,50);
+ 
+    
+    test(wa*0.5,200, wa, 100);
+    rotate(-wa)
+    test(wa-4,150,wa/20,100);
+    rotate(wa)
+    test(wa/10,wa,wa-5,60);
 
-    function test(w,h, x, y){        
+    function test(w,h, x, y){ 
+        rectMode(CENTER); 
+        ellipseMode(CENTER)
+        let wa =x++      
     noStroke()
     fill(255, 0, 0)    
     rect(x, y, w , h);
+    rotate(frameRate)
     fill(255, 150 ,0)
-    ellipse(x*2, y*2, h/1.5, w/1.5)
+    ellipse(x, y, h/1.5, w/1.5)
 
     }
     
