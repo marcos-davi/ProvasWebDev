c27c2f0c5840878081c89b5aeba207941e2c7132
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2023-01-02 21:11:57 +0100

    Curso React video 03

--- CURSO-DE-REACT/intro.js
+++ CURSO-DE-REACT/intro.js
@@ -1,6 +1,7 @@
 const containerElement = document.getElementById("container");
 
 //CREANDO Y RENDEREZINADO UN ELEMENTO CON JAVASCRIPT PURO
+
 // const headerElement = document.createElement("h1");
 // headerElement.textContent =  " Curso práctico de React: 02 - javascript puro";
 // containerElement.appendChild(headerElement);
@@ -10,3 +11,4 @@ const headerElement = React.createElement("h1",{
     children: "Curso de React: 02 - React"
 });
 ReactDOM.render(headerElement, containerElement);
+ 
